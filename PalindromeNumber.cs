0bc19901d5e828c7ee7ba9e3b60b7cb1dd04fe63
Youva LOUNAS <youva.lounas.97@gmail.com>
2024-10-07 22:17:06 +0200

    Add other solution

--- PalindromeNumber.cs
+++ PalindromeNumber.cs
@@ -2,6 +2,26 @@ using System;
 		
 public class Program
 {
+	// Méthode 1
+	public static bool IsPalindrome(int x) 
+	{    
+		var xString = x.ToString();
+		var length = xString.Length;
+		var isPalindrome = false;
+		
+		if(xString.Length == 1) return true;
+		
+		for(int i=0; i<(length - (i+1)); i++) 
+		{
+			isPalindrome = xString[i] == xString[length - (i+1)];
+            if(!isPalindrome) return false; 
+		}
+		
+		return isPalindrome;
+    }
+
+	/* 
+	// Méthode 2
 	public static bool IsPalindrome(int x) 
 	{
 		//Convertir x en string 
@@ -20,15 +40,18 @@ public class Program
 		
 		return isPalindrome;
     }
+	*/
 	
 	public static void Main()
 	{
 		var x = 121;
-		var y = 122; 
+		var y = 22; 
 		var z = 1;
+		var w = 100021;
 		
 		Console.WriteLine($"x is a palindrome : {IsPalindrome(x)}");
 		Console.WriteLine($"y is a palindrome : {IsPalindrome(y)}");
 		Console.WriteLine($"z is a palindrome : {IsPalindrome(z)}");
+		Console.WriteLine($"w is a palindrome : {IsPalindrome(w)}");
     }
 }
